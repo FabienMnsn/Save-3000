a3bc5453629828b6f02c53230cd9c28521ae7d67
U <fabien.manson@free.fr>
2023-05-31 23:11:45 +0200

    poc in progress

--- myCustomThread.py
+++ myCustomThread.py
@@ -1,53 +1,89 @@
 from threading import Thread
 from threading import get_ident
 from threading import Event
+from threading import Lock
 from time import sleep
 from multiprocessing import cpu_count
 from concurrent.futures import ThreadPoolExecutor
 
 
-
 class CustomThread(Thread):
-	def __init__(self, wk_ev, wk_lk, dataset, b_size):
+	def __init__(self, wk_ev, wk_lk, dataset, b_size, print_lock):
 		Thread.__init__(self)
 		self.worker_event = wk_ev
 		self.worker_lock = wk_lk
 		self.dataset = dataset
 		self.batch_size = b_size
 		self.working_list = []
-		signal.signal(signal.SIGSTOP, signal_handler)
+		self.print_lock = print_lock
 
 
 	def run(self):
-		#signal.signal(signal.SIGSTOP, signal_handler)
 		while True:
-			if(self.event.is_set()):
+			#sleep(1)
+			#self.print_lock.acquire()
+			#print("[", get_ident(), "] : Working list :", self.working_list)
+			#self.print_lock.release()
+
+			if(self.worker_event.is_set()):
 				self.signal_handler()
-				break
+
 			if(len(self.working_list) > 0):
 				# do copy file
 				for element in self.working_list:
-					print("[%s] : Copying file %s" % get_ident(), element)
+					if(self.worker_event.is_set()):
+						self.print_lock.acquire()
+						print("[", get_ident(), "] : Worker Received Signal")
+						self.print_lock.release()
+						self.signal_handler()
+					#sleep(0.3)
+					#self.print_lock.acquire()
+					self.working_list.remove(element)
+					#print("[", get_ident(), "] : Copying file", element)
+					#self.print_lock.release()
 			else:
 				if(len(self.dataset) > 0):
+					#self.print_lock.acquire()
+					#print("[", get_ident(), "] : Grabbing data from dataset")
+					#self.print_lock.release()
 					for i in range(self.batch_size):
 						try:
-							print("[%s] : Acquire lock" % get_ident())
+							#self.print_lock.acquire()
+							#print("[", get_ident(), "] : Acquire lock")
+							#self.print_lock.release()
 							self.worker_lock.acquire()
 							self.working_list.append(self.dataset.pop())
+							if(self.worker_lock.locked()):
+								self.worker_lock.release()
+								#self.print_lock.acquire()
+								#print("[", get_ident(), "] : Release lock")
+								#self.print_lock.release()
 						except KeyError:
-							print("[%s] : Set is empty, breaking" % get_ident())
+							#self.print_lock.acquire()
+							#print("[", get_ident(), "] : Set is empty, breaking")
+							#self.print_lock.release()
 							break
 						finally:
-							print("[%s] : Release lock" % get_ident())
-							self.worker_lock.release()
+							#self.print_lock.acquire()
+							#print("[", get_ident(), "] : Release lock (finally)")
+							#self.print_lock.release()
+							if(self.worker_lock.locked()):
+								self.worker_lock.release()
 				else:
-					print("[%s] : Quitting main loop" % get_ident())
+					self.print_lock.acquire()
+					print("[", get_ident(), "] : Quitting main loop")
+					self.print_lock.release()
 					break
+		self.print_lock.acquire()
+		print("[", get_ident(), "] : ---------------- Worker's Job Completed")
+		self.print_lock.release()
+			
 
 
 	def signal_handler(self):
-		printprint("[%s] : Signal Received Quitting" % get_ident())
+		self.print_lock.acquire()
+		print("[", get_ident(), "] : Signal Received Quitting")
+		self.print_lock.release()
 		exit(0)
 
 
@@ -59,22 +95,31 @@ class CustomThread(Thread):
 
 
 class CustomThreadPool(Thread):
-	def __init__(self, tp_ev, wk_ev, dataset, b_size):
+	def __init__(self, tp_ev, dataset, b_size, print_lock):
 		Thread.__init__(self)
 		self.threadpool_event = tp_ev
-		self.worker_event = wk_ev
+		self.worker_event = Event()
+		self.worker_lock = Lock()
 		self.dataset = dataset
 		self.workerList = []
 		self.batch_size = b_size
-		signal.signal(signal.SIGSTOP, signal_handler)
+		self.print_lock = print_lock
+		#signal.signal(signal.SIGSTOP, signal_handler)
 
 
 	def run(self):
 		print("Start Worker Pool")
 		for i in range(cpu_count()):
-			self.workerList.append(CustomThread())
+			# CustomThread(self, wk_ev, wk_lk, dataset, b_size)
+			w = CustomThread(self.worker_event, self.worker_lock, self.dataset, self.batch_size, self.print_lock)
+			w.start()
+			self.workerList.append(w)
 
-		while not self.threadpool_event.is_set():
+		while True:
+			print("--------------------------------> THREAD POOL EVENT :", self.threadpool_event.is_set())
+			if(self.threadpool_event.is_set()):
+				self.signal_handler()
+				break
 			sleep(0.3)
 	
 
@@ -111,13 +156,38 @@ class CustomThreadPool(Thread):
 
 
 	def signal_handler(self):
-		print(vars(self.threadPool))
-		print(self.threadPool._work_queue.qsize())
-		for t in self.threadPool._threads:
-			print(vars(t))
-			t.cancel()
-			print(vars(t))
-		#for r in self.threadPool._work_queue:
-		#	print(type(r))
-		print("Receiving Signal -> Exiting Thread Pool")
+		self.worker_event.set()
+		for worker in self.workerList:
+			worker.join()
+		return
+
+
+
+
+
+
+
+class WatcherThread(Thread):
+	def __init__(self, w_ev, dataset, print_lock):
+		Thread.__init__(self)
+		self.worker_event = w_ev
+		self.dataset = dataset
+		self.print_lock = print_lock
+
+
+	def run(self):
+		while True:
+			if(len(self.dataset) == 0 or self.worker_event.is_set()):
+				self.signal_handler()
+				break
+			self.print_lock.acquire()
+			print("DATA_SET SIZE :", len(self.dataset))
+			self.print_lock.release()
+			sleep(1)
+
+
+	def signal_handler(self):
+		self.print_lock.acquire()
+		print("[", get_ident(), "] : Signal Received Watcher Quitting")
+		self.print_lock.release()
 		exit(0)
--- poc.py
+++ poc.py
@@ -1,5 +1,5 @@
-from threading import Event
-from myCustomThread import CustomThread
+from threading import Event, Lock
+from myCustomThread import CustomThread, WatcherThread
 from myCustomThread import CustomThreadPool
 from time import sleep
 from multiprocessing import cpu_count
@@ -10,28 +10,36 @@ from multiprocessing import cpu_count
 def main():
 	#c_set = {"1", "2", "3", "4", "5", "6", "7", "8"}
 	c_set = {x for x in range(1000)}
-	print(c_set)
-	ev = Event()
+	#ev = Event()
 	#t1 = CustomThread(ev, c_set)
 	#t1.start()
-	#t2 = CustomThreadPool(ev, c_set, cpu_count(), 2)
-	#t2.start()
-	print("Main sleeping")
+
+	thread_pool_event = Event()
+
+	print_lock = Lock()
+
+	t1 = WatcherThread(thread_pool_event, c_set, print_lock)
+	t1.start()
+
+	t2 = CustomThreadPool(thread_pool_event, c_set, 10, print_lock)
+	t2.start()
 	#sleep(5)
-	ev.set()
-	print(c_set)
+	#ev.set()
 
-	c = {1}
-	print(c.pop())
-
-	try:
-		c.pop()
-	except Exception as e:
-		if("pop from an empty set" in str(e)):
-			print("SAME")
-		else:
-			print("DIFFERENT")
-	finally:
-		print("finally")
+	#sleep(5)
+	#print("SENDING EVENT")
+	#thread_pool_event.set()
+	#c = {1}
+	#print(c.pop())
+	#
+	#try:
+	#	c.pop()
+	#except Exception as e:
+	#	if("pop from an empty set" in str(e)):
+	#		print("SAME")
+	#	else:
+	#		print("DIFFERENT")
+	#finally:
+	#	print("finally")
 if __name__ == '__main__':
 	main()
