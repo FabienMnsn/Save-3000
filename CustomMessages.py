b1f39b5810d2ecd56fb10e108168428a876d98c6
U <fabien.manson@free.fr>
2023-06-04 11:43:05 +0200

    Update CustomMessages.py

--- CustomMessages.py
+++ CustomMessages.py
@@ -2,22 +2,42 @@ from tkinter import *
 
 
 class CustomError(Toplevel):
-	def __init__(self, msg):
+	def __init__(self, msg, root):
 		Toplevel.__init__(self)
-		self.error_label = Label(self, text=msg)
-		self.error_label.pack(side=TOP, expand=True, fill=X, padx=5, pady=5)
-		self.error_button = Button(self, command=self.destroy, text="OK")
-		self.error_button.pack(side=TOP, expand=False, padx=5, pady=5)
+		self.title("Erreur")
+		self.main_theme_light = "grey85"
+		self.main_theme = "grey35"
+		self.parent = root
+
+		self.minsize(230, 120)
+		
+		self.resizable(False, False)
 		self.attributes('-topmost', 'true')
-		self.bind("<Configure>", self.getSize)
+		#self.protocol("WM_DELETE_WINDOW", self.on_closing)
+		self.error_label = Label(self, text=msg, wraplength=200, justify=LEFT)
+		self.error_label.pack(side=TOP, expand=True, fill=X, padx=5, pady=5)
+
+		self.button_frame = Frame(self, bg=self.main_theme_light, width=50)
+		self.button_frame.pack(side=BOTTOM, expand=False, fill=X, padx=0, pady=0)
+
+		self.error_button = Button(self.button_frame, command=self.on_button_ok_click, text="OK", width=8)
+		self.error_button.pack(side=TOP, expand=False, padx=5, pady=5)
+
+		#self.setPosition()
 
 
-	def getSize(self):
-		print(self.winfo_width(), self.winfo_height())
+	def setPosition(self):
+		self.geometry("+%d+%d" % (self.parent.winfo_x() + self.parent.winfo_width()/2 - self.winfo_width()/2, self.parent.winfo_y() + self.parent.winfo_height()/2 - self.winfo_height()/2))
 
+
+	def on_button_ok_click(self):
+		self.destroy()
 
 
 if __name__ == '__main__':
 	w = Tk()
-	CustomError("Ceci est un message d'erreur !")
+	w.geometry("700x400")
+	CustomError("Ceci est un message d'erreur vraiment très très long hein il n'est pas pret de se terminer !", w)
+	print(res)
 	w.mainloop()
+
